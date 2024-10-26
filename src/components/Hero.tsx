3e478fbeae12935005bac7f1528e13ee059c9e4e
mrfaizee12 <faizee956@gmail.com>
2024-10-27 02:26:22 +0500

    updated hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,22 +1,48 @@
-import React from 'react'
+// import React from 'react';
+import Link from 'next/link';
 import Navbar from './Navbar';
+import Typewriter from 'typewriter-effect';
 
 const Hero = () => {
   return (
-    <div id='hero'>
-        <Navbar/>
-      <div className='hero-container'>
-        <div className='hide-on-small'></div>
-        <div className='large-text'>
+    <div id="hero">
+      <Navbar />
+      <div className="hero-container">
+        <div className="hide-on-small"></div>
+        <div className="large-text">
           <div>
-            <p>I&#39;m</p>
-            <p>Faizan</p>
-            <p>Anjum</p>
+            <p>Hi there!</p>
+            <div>
+              <p>
+                I&#39;m <span className="name">Faizan Anjum</span>
+              </p>
+              <Typewriter
+                options={{
+                  strings: [
+                    'Frontend Developer',
+                    'Backend Developer',
+                    'Certified Cloud Specialist',
+                    'Generative AI Enthusiast',
+                  ],
+                  autoStart: true,
+                  loop: true,
+                  wrapperClassName: 'typewriter-text', // For custom styling
+                }}
+              />
+            </div>
+            <div className="button-container">
+              <Link href="#contact" passHref>
+                <button className="btn hire-me">Hire Me</button>
+              </Link>
+              <Link href="https://milestone01-2.vercel.app/" passHref>
+                <button className="btn view-cv">View CV</button>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Hero;
