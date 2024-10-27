128ceb0005f11b3c76c8df8d24fe14ad7fc9d165
mrfaizee12 <faizee956@gmail.com>
2024-10-27 21:03:03 +0500

    Updated font sizes and custom CSS styles for improved readability

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,16 +9,16 @@ const Footer = () => {
     <div className="footer-container">
       <p className="footer-text">Follow me on social media</p>
       <div className="footer-icons">
-        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" className="footer-icon">
+        <a href="https://www.instagram.com/welcometofaizeecity/" target="_blank" rel="noopener noreferrer" className="footer-icon">
           <IoLogoInstagram />
         </a>
-        <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" className="footer-icon">
+        <a href="https://web.facebook.com/faizan.anjum.7334" target="_blank" rel="noopener noreferrer" className="footer-icon">
           <IoLogoFacebook />
         </a>
-        <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer" className="footer-icon">
+        <a href="https://www.linkedin.com/in/faizan-anjum-a044912b8/" target="_blank" rel="noopener noreferrer" className="footer-icon">
           <FaLinkedin />
         </a>
-        <a href="https://github.com" target="_blank" rel="noopener noreferrer" className="footer-icon">
+        <a href="https://github.com/mrfaizee12" target="_blank" rel="noopener noreferrer" className="footer-icon">
           <SiGithub />
         </a>
       </div>
